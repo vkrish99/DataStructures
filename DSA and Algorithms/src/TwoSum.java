074c7de507e81d1bef24a94fbf320c484cbacef5
vkrish99 <v.krish191@gmail.com>
2025-12-27 00:41:18 +0530

    DSA- Two Sum commit

---
+++ DSA and Algorithms/src/TwoSum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x)) {
+                return new int[]{map.get(x), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 18;
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("Two Sum Indices: " + result[0] + ", " + result[1]);
+    }
+}
